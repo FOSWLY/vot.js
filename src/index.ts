7bd562d8fbf02961286858618007cdd225ad3445
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-07-28 05:35:10 +0300

    types moved to separated file

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,5 @@ export { yandexProtobuf } from "./protobuf";
 
 export { default, VOTWorkerClient } from "./client";
 
-export * as ClientType from "./types/client";
-export * as YandexType from "./types/yandex";
-export * as VOTBackendType from "./types/vot";
-export * as YandexProtobufType from "./protos/yandex";
-
-export * as MailRuType from "./types/helpers/mailru";
-export * as WeverseType from "./types/helpers/weverse";
-export * as KodikType from "./types/helpers/kodik";
-export * as PatreonType from "./types/helpers/patreon";
-
+export * as VOTTypes from "./types/index";
 export * as VOTConsts from "./consts";
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export * as YandexProtobufType from "../protos/yandex";
+
+export * as ClientType from "./client";
+export * as YandexType from "./yandex";
+export * as VOTBackendType from "./vot";
+export * as UtilsType from "./utils";
+
+export * as MailRuType from "./helpers/mailru";
+export * as WeverseType from "./helpers/weverse";
+export * as KodikType from "./helpers/kodik";
+export * as PatreonType from "./helpers/patreon";
+export * as KickType from "./helpers/kick";
