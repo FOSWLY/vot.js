83a76ca8c08355939c426732da06911c7e76f5b6
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-08-27 19:45:06 +0300

    set readonly items

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -32,41 +32,41 @@ export * as TwitchHelper from "./twitch";
  */
 export default class VideoHelper {
   /** @source */
-  static [VideoService.mailru] = new MailRuHelper();
+  static readonly [VideoService.mailru] = new MailRuHelper();
 
   /** @source */
-  static [VideoService.weverse] = new WeverseHelper();
+  static readonly [VideoService.weverse] = new WeverseHelper();
 
   /** @source */
-  static [VideoService.kodik] = new KodikHelper();
+  static readonly [VideoService.kodik] = new KodikHelper();
 
   /** @source */
-  static [VideoService.patreon] = new PatreonHelper();
+  static readonly [VideoService.patreon] = new PatreonHelper();
 
   /** @source */
-  static [VideoService.reddit] = new RedditHelper();
+  static readonly [VideoService.reddit] = new RedditHelper();
 
   /** @source */
-  static [VideoService.bannedvideo] = new BannedVideoHelper();
+  static readonly [VideoService.bannedvideo] = new BannedVideoHelper();
 
   /** @source */
-  static [VideoService.kick] = new KickHelper();
+  static readonly [VideoService.kick] = new KickHelper();
 
   /** @source */
-  static [VideoService.appledeveloper] = new AppleDeveloperHelper();
+  static readonly [VideoService.appledeveloper] = new AppleDeveloperHelper();
 
   /** @source */
-  static [VideoService.epicgames] = new EpicGamesHelper();
+  static readonly [VideoService.epicgames] = new EpicGamesHelper();
 
   /** @source */
-  static [VideoService.nineanimetv] = new NineAnimeTVHelper();
+  static readonly [VideoService.nineanimetv] = new NineAnimeTVHelper();
 
   /** @source */
-  static [VideoService.odysee] = new OdyseeHelper();
+  static readonly [VideoService.odysee] = new OdyseeHelper();
 
   /** @source */
-  static [VideoService.twitch] = new TwitchHelper();
+  static readonly [VideoService.twitch] = new TwitchHelper();
 
   /** @source */
-  static [VideoService.coursehunter] = new CoursehunterHelper();
+  static readonly [VideoService.coursehunter] = new CoursehunterHelper();
 }
