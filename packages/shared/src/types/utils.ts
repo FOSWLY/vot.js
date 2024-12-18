82e14cab72e8f2f802807c9bb4e09003ac789670
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-18 15:20:27 +0300

    chore: graphql placeholder type

--- packages/shared/src/types/utils.ts
+++ packages/shared/src/types/utils.ts
@@ -2,3 +2,7 @@ export type ISODate = string;
 export type AtLeast<T, K extends keyof T> = Partial<T> & Pick<T, K>;
 export type TinyInt = 0 | 1;
 export type StringBoolean = "true" | "false";
+export type GraphQL<T> = {
+  data: T;
+  extensions?: unknown;
+};
