341ed9e91fe10886a885f2f5beefa1be236f7a50
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-22 21:30:48 +0300

    feat: added export artstation and loom helper types

--- packages/ext/src/types/index.ts
+++ packages/ext/src/types/index.ts
@@ -4,3 +4,6 @@ export * as ServiceType from "./service";
 export * as VKType from "./helpers/vk";
 export * as LinkedinType from "./helpers/linkedin";
 export * as DouyinHelper from "./helpers/douyin";
+export * as LoomHelper from "./helpers/loom";
+export * as ArtstationHelper from "./helpers/artstation";
+export * as BaseHelper from "./helpers/base";
