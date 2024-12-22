79dd0de62fa461d6ff20fe23cecd570869c0cb3e
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-22 21:29:51 +0300

    feat: add support twitter feed

--- packages/ext/src/helpers/twitter.ts
+++ packages/ext/src/helpers/twitter.ts
@@ -3,6 +3,17 @@ import { BaseHelper } from "./base";
 export default class TwitterHelper extends BaseHelper {
   // eslint-disable-next-line @typescript-eslint/require-await
   async getVideoId(url: URL) {
-    return /status\/([^/]+)/.exec(url.pathname)?.[1];
+    const videoId = /status\/([^/]+)/.exec(url.pathname)?.[1];
+    if (videoId) {
+      return videoId;
+    }
+
+    const postEl = this.video?.closest('[data-testid="tweet"]');
+    const newLink = (
+      postEl?.querySelector('a[role="link"][aria-label]') as
+        | HTMLLinkElement
+        | undefined
+    )?.href;
+    return newLink ? /status\/([^/]+)/.exec(newLink)?.[1] : undefined;
   }
 }
