b08bac429cffd92262a6d58b861696bc96090236
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-25 13:33:51 +0300

    feat: add getVideoId by player

--- packages/ext/src/helpers/douyin.ts
+++ packages/ext/src/helpers/douyin.ts
@@ -7,15 +7,22 @@ import { RequestLang } from "@vot.js/shared/types/data";
 import { availableLangs } from "@vot.js/shared/consts";
 
 export default class DouyinHelper extends BaseHelper {
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async getVideoData(videoId: string) {
+  getPlayer() {
     // @ts-expect-error var from page scripts
     if (typeof player === "undefined") {
       return undefined;
     }
 
     // @ts-expect-error var from page scripts
-    const xgPlayer = player as Douyin.Player; // window.player
+    return player as Douyin.Player;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async getVideoData(videoId: string) {
+    const xgPlayer = this.getPlayer();
+    if (!xgPlayer) {
+      return undefined;
+    }
 
     const { url: sources, duration, lang, isLive: isStream } = xgPlayer.config;
     if (!sources) {
@@ -41,6 +48,16 @@ export default class DouyinHelper extends BaseHelper {
 
   // eslint-disable-next-line @typescript-eslint/require-await
   async getVideoId(url: URL) {
-    return /video\/([\d]+)/.exec(url.pathname)?.[0];
+    const pathId = /video\/([\d]+)/.exec(url.pathname)?.[0];
+    if (pathId) {
+      return pathId;
+    }
+
+    const xgPlayer = this.getPlayer();
+    if (!xgPlayer) {
+      return undefined;
+    }
+
+    return xgPlayer.config.vid;
   }
 }
