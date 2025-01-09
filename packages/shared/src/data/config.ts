b139fb2809fa907c40f77b6ed1533f3a0904b702
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-01-09 13:44:39 +0300

    chore: updated user agent

--- packages/shared/src/data/config.ts
+++ packages/shared/src/data/config.ts
@@ -9,11 +9,11 @@ export default {
   hostWorker: "vot-worker.toil.cc",
   mediaProxy: "media-proxy.toil.cc",
   userAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 YaBrowser/24.12.0.0 Safari/537.36",
   componentVersion: "24.12.2.856",
   hmac: "bt8xH3VOlb4mqf0nqAibnDOoiPlXsisf",
   defaultDuration: 343,
   minChunkSize: 5295308,
   loggerLevel: 1,
-  version: "2.1.5",
+  version: "2.1.6",
 } as ConfigSchema;
