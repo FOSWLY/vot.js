0266993191378614e71260aa46b7af3988927550
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-01-09 13:45:28 +0300

    chore: added sec-ch-ua headers

--- packages/ext/src/client.ts
+++ packages/ext/src/client.ts
@@ -2,15 +2,31 @@ import VOTCoreClient, {
   VOTWorkerClient as VOTCoreWorkerClient,
 } from "@vot.js/core/client";
 import type { VOTOpts } from "@vot.js/core/types/client";
+import config from "@vot.js/shared/config";
+
+const { componentVersion } = config;
+
+export const secHeaders = {
+  "sec-ch-ua": `"Chromium";v="130", "YaBrowser";v="${componentVersion.slice(0, 5)}", "Not?A_Brand";v="99", "Yowser";v="2.5"`,
+  "sec-ch-ua-full-version-list": `"Chromium";v="130.0.6723.152", "YaBrowser";v="${componentVersion}", "Not?A_Brand";v="99.0.0.0", "Yowser";v="2.5"`,
+};
 
 export default class VOTClient extends VOTCoreClient {
   constructor(opts?: VOTOpts) {
     super(opts);
+    this.headers = {
+      ...secHeaders,
+      ...this.headers,
+    };
   }
 }
 
 export class VOTWorkerClient extends VOTCoreWorkerClient {
   constructor(opts?: VOTOpts) {
     super(opts);
+    this.headers = {
+      ...secHeaders,
+      ...this.headers,
+    };
   }
 }
