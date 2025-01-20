659a722f4061c9686fcfd6eba6f003cace598116
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-01-21 02:27:18 +0300

    chore: bump version

--- packages/shared/src/data/config.ts
+++ packages/shared/src/data/config.ts
@@ -10,10 +10,10 @@ export default {
   mediaProxy: "media-proxy.toil.cc",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 YaBrowser/24.12.0.0 Safari/537.36",
-  componentVersion: "24.12.2.856",
+  componentVersion: "24.12.3.780",
   hmac: "bt8xH3VOlb4mqf0nqAibnDOoiPlXsisf",
   defaultDuration: 343,
   minChunkSize: 5295308,
   loggerLevel: 1,
-  version: "2.1.10",
+  version: "2.1.11",
 } as ConfigSchema;
