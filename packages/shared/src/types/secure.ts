db482381430df8b119c7af691cd91823f5eba577
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-01-25 18:56:55 +0300

    chore: added new sec types and session module

--- packages/shared/src/types/secure.ts
+++ packages/shared/src/types/secure.ts
@@ -1,6 +1,11 @@
-export type SecType = "Vtrans" | "Vsubs" | "Summary";
+// Summary for summarization and neuroedit, Ya-Summary for neuroapi
+export type SecType = "Vtrans" | "Vsubs" | "Summary" | "Ya-Summary";
 export type HashName = "SHA-256" | "SHA-1";
-export type SessionModule = "video-translation" | "summarization";
+export type SessionModule =
+  | "video-translation"
+  | "summarization"
+  | "neuroapi"
+  | "neuroedit";
 
 export type ClientSession = {
   expires: number; // seconds lifetime from response (e.g. 3600)
