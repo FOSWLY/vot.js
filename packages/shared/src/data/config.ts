e5543795f9aff76aed65f3719b958e7834904d1b
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-04-21 23:03:17 +0300

    chore: bump component version

--- packages/shared/src/data/config.ts
+++ packages/shared/src/data/config.ts
@@ -9,8 +9,8 @@ export default {
   hostWorker: "vot-worker.toil.cc",
   mediaProxy: "media-proxy.toil.cc",
   userAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 YaBrowser/25.2.0.0 Safari/537.36",
-  componentVersion: "25.2.5.953",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 YaBrowser/25.4.0.0 Safari/537.36",
+  componentVersion: "25.4.0.1746",
   hmac: "bt8xH3VOlb4mqf0nqAibnDOoiPlXsisf",
   defaultDuration: 343,
   minChunkSize: 5295308,
