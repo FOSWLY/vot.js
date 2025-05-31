6149e4e997a434f233509ef04474e42517a7e490
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-05-31 17:50:33 +0300

    chore: updated get actual version logic

--- scripts/update-config.ts
+++ scripts/update-config.ts
@@ -15,7 +15,7 @@ async function rewriteConfig(data: typeof config) {
   const rawCode = `// This file is auto-generated.
     // All comments and any code are deleted when the componentVersion is updated.
     // Write comments in scripts/update-config.ts
-    import { ConfigSchema } from "../types/data";
+    import type { ConfigSchema } from "../types/data";
 
     export default ${JSON.stringify(data, null, 2)} as ConfigSchema`;
 
@@ -32,14 +32,13 @@ async function getActualVersion(version: string) {
   const urlParams = new URLSearchParams({
     version,
     custo: "yes",
-    reason: "browser_updater",
+    manual: "yes",
     uid: crypto.randomUUID().toUpperCase(),
     os_arch: "x86_64",
   }).toString();
-  const res = await fetch(
-    `https://api.browser.yandex.ru/update-info/browser/yandex/win-yandex.rss?${urlParams}`,
-    { headers: { "User-Agent": config.userAgent } },
-  );
+  const url = `https://api.browser.yandex.ru/update-info/browser/yandex/win-yandex.rss?${urlParams}`;
+  console.log(`Fetching Yandex version info from: ${url}`);
+  const res = await fetch(url, { headers: { "User-Agent": config.userAgent } });
 
   let content = await res.text();
   if (!content.startsWith("<?xml")) {
@@ -53,10 +52,9 @@ async function getActualVersion(version: string) {
   const versionData = parseFromString(content);
   const versions = versionData.getElementsByTagName("version:version");
   const newVersion = versions?.[0]?.textContent;
-  if (newVersion === "25.4.0.1971") {
-    // temp fix. semver: 25.4.0.1971 > 25.4.0.1973
-    return version;
-  }
+  console.log(
+    `Old version: ${config.componentVersion} | New version: ${newVersion}`,
+  );
 
   const isNewer = semver.satisfies(newVersion, `^${version}`);
   if (!isNewer) {
