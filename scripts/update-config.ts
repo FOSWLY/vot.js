42bbdc80dd95dcd7d58a29af930dd34849f2e37b
Toil <62353659+ilyhalight@users.noreply.github.com>
2026-09-17 01:39:51 +0300

    chore: cache for yandex component version, better logs

--- scripts/update-config.ts
+++ scripts/update-config.ts
@@ -9,7 +9,15 @@ import { version } from "../package.json";
 import config from "../packages/shared/src/data/config";
 
 const CONFIG_PATH = "/packages/shared/src/data/config.ts";
-const CONFIG_ABS_PATH = path.join(__dirname, "..", CONFIG_PATH);
+const ROOT_PATH = path.join(__dirname, "..");
+const CONFIG_ABS_PATH = path.join(ROOT_PATH, CONFIG_PATH);
+const CACHE_PATH = path.join(ROOT_PATH, ".yabrover_cache");
+const MAX_CACHE_AGE = 10 * 60 * 1000; // 10 minutes
+
+type CachedData = {
+  version: string;
+  timestamp: number;
+};
 
 async function rewriteConfig(data: typeof config) {
   const formatResult = await format(
@@ -29,7 +37,7 @@ async function rewriteConfig(data: typeof config) {
   console.log("Successfully rewrited config");
 }
 
-async function getActualVersion(version: string) {
+async function fetchYandexRSSVersion() {
   const urlParams = new URLSearchParams({
     version,
     custo: "yes",
@@ -38,7 +46,7 @@ async function getActualVersion(version: string) {
     os_arch: "x86_64",
   }).toString();
   const url = `https://api.browser.yandex.ru/update-info/browser/yandex/win-yandex.rss?${urlParams}`;
-  console.log(`Fetching Yandex version info from: ${url}`);
+  console.log(`Fetching Yandex version info from "${url}"`);
   const res = await fetch(url, { headers: { "User-Agent": config.userAgent } });
 
   let content = await res.text();
@@ -52,9 +60,69 @@ async function getActualVersion(version: string) {
 
   const versionData = parseFromString(content);
   const versions = versionData.getElementsByTagName("version:version");
-  const newVersion = versions?.[0]?.textContent;
+  return versions?.[0]?.textContent;
+}
+
+async function readCachedYandexVersion() {
+  const file = Bun.file(CACHE_PATH);
+  if (!(await file.exists())) {
+    return null;
+  }
+
+  try {
+    const content = (await file.json()) as CachedData;
+    if (content.timestamp + MAX_CACHE_AGE < Date.now()) {
+      console.log("Cached Yandex version is expired, fetching new version...");
+      return null;
+    }
+
+    if (!content.version) {
+      console.error(
+        "Cached Yandex version is invalid, fetching new version...",
+      );
+      return null;
+    }
+
+    return content.version;
+  } catch (err) {
+    console.error("Failed to read cached Yandex version:", err);
+  }
+
+  return null;
+}
+
+async function saveCachedYandexVersion(version: string) {
+  const file = Bun.file(CACHE_PATH);
+  const data: CachedData = {
+    version,
+    timestamp: Date.now(),
+  };
+
+  try {
+    await Bun.write(file, JSON.stringify(data, null, 2));
+    console.log(`Saved Yandex version "${version}" to cache`);
+  } catch (err) {
+    console.error("Failed to save cached Yandex version:", err);
+  }
+}
+
+async function getActualVersion(version: string) {
+  const cachedVersion = await readCachedYandexVersion();
+  if (cachedVersion) {
+    console.log(`Using cached Yandex version: "${cachedVersion}"`);
+    return cachedVersion;
+  }
+
+  const newVersion = await fetchYandexRSSVersion();
+  await saveCachedYandexVersion(newVersion);
+  const isSame = config.componentVersion === newVersion;
+  if (isSame) {
+    console.log(`Yandex version is up-to-date: "${newVersion}"`);
+    return newVersion;
+  }
+
   console.log(
-    `Old version: ${config.componentVersion} | New version: ${newVersion}`,
+    `Old version: "${config.componentVersion}" | New version: "${newVersion}"`,
   );
 
   const isNewer = semver.satisfies(newVersion, `^${version}`);
@@ -69,7 +137,7 @@ const yaActualVersion = await getActualVersion(config.componentVersion);
 config.componentVersion = yaActualVersion;
 
 const chromiumRevision = yaActualVersion.split(".").at(-1);
-if (chromiumRevision) {
+if (chromiumRevision && chromiumRevision !== config.chromiumRevision) {
   console.log(`Updating chromiumRevision to: ${chromiumRevision}`);
   config.chromiumRevision = chromiumRevision;
 }
