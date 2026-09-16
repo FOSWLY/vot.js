b20f8ee05d338fc712e97b4614c88a774d92b6f1
Toil <62353659+ilyhalight@users.noreply.github.com>
2026-09-17 01:41:08 +0300

    chore: bump componentVersion

--- packages/shared/src/data/config.ts
+++ packages/shared/src/data/config.ts
@@ -7,14 +7,14 @@ import type { LoggerLevel } from "../types/logger";
 export default {
   host: "api.browser.yandex.ru",
   hostWorker: "vot-worker.toil.cc",
-  mediaProxy: "media-proxy.transly.eu.cc",
+  mediaProxy: "media-proxy.toil.cc",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 YaBrowser/26.8.0.0 Safari/537.36",
-  componentVersion: "26.8.2.1064",
-  chromiumRevision: "1064",
+  componentVersion: "26.8.3.971",
+  chromiumRevision: "971",
   hmac: "bt8xH3VOlb4mqf0nqAibnDOoiPlXsisf",
   defaultDuration: 310,
   minChunkSize: 5295308,
   loggerLevel: 1 as LoggerLevel,
-  version: "3.0.4",
+  version: "3.1.0",
 } satisfies ConfigSchema;
